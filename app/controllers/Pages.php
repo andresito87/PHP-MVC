a754953b78ea91fb59ea6b8df99c4a2b89d8efda
Andres <andresito87>
2023-07-11 17:19:12 +0200

    first commit after merge with develop branch

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,25 +4,15 @@
     {
         public function __construct()
         {
-<<<<<<< HEAD
-            $this->postModel = $this->model('Post');
-=======
 
->>>>>>> 4e8c5d2 (first commit after merge with develop branch)
         }
 
         public function index()
         {
-<<<<<<< HEAD
-            $posts= $this->postModel->getPosts();
-            $data = ['title' => 'Welcome',
-                     'posts' => $posts];
-=======
             $posts = $this->postModel->getPosts();
             $data = [
                 'title' => 'Welcome'
             ];
->>>>>>> 4e8c5d2 (first commit after merge with develop branch)
 
             $this->view('pages/index', $data);
         }

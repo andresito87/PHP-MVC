5bc5795c6422b2b024a855585c181ee400ca545a
Andres <andresito87>
2023-07-11 17:21:02 +0200

    first commit after merge with develop branch

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,14 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 <h1><?php echo $data['title'] ?></h1>
-<<<<<<< HEAD
-<ul>
-    <?php foreach($data['posts'] as $post) : ?>
-        <li><?php echo $post->name; ?></li>
-    <?php endforeach; ?>
-</ul>
-=======
 <p>This is the PHP MVC framework. Please refer to the docs on how to use it.</p>
->>>>>>> 4e8c5d2 (first commit after merge with develop branch)
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>

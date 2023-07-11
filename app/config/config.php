0f3f428620373cbe29033d4ea4b76f7973d69f8a
Andres <andresito87>
2023-07-11 15:58:56 +0200

    first commit after merge with develop branch

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,25 @@
 <?php
     //DB Params
     define('DB_HOST', 'localhost');
+<<<<<<< HEAD
     define('DB_USER', 'root');
     define('DB_PASS', '');
     define('DB_NAME', 'php-mvc');
+=======
+    define('DB_USER', '_YOUR_USER_');
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DBNAME_');
+>>>>>>> 4e8c5d2 (first commit after merge with develop branch)
 
     //App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     //URL Root
+<<<<<<< HEAD
     define('URLROOT', 'http://localhost/php-mvc');
     //Site Name
     define('SITENAME', 'PHP MVC');
+=======
+    define('URLROOT', '_YOUR_URL_');
+    //Site Name
+    define('SITENAME', '_YOUR_SITENAME_');
+>>>>>>> 4e8c5d2 (first commit after merge with develop branch)
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,14 +4,25 @@
     {
         public function __construct()
         {
+<<<<<<< HEAD
             $this->postModel = $this->model('Post');
+=======
+
+>>>>>>> 4e8c5d2 (first commit after merge with develop branch)
         }
 
         public function index()
         {
+<<<<<<< HEAD
             $posts= $this->postModel->getPosts();
             $data = ['title' => 'Welcome',
                      'posts' => $posts];
+=======
+            $posts = $this->postModel->getPosts();
+            $data = [
+                'title' => 'Welcome'
+            ];
+>>>>>>> 4e8c5d2 (first commit after merge with develop branch)
 
             $this->view('pages/index', $data);
         }
